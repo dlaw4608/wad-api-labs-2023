95030dc4f527b7aae11a2f5e02e0f46e2e2080e8
dlaw4608 <dlawton032@gmail.com>
2023-12-18 19:46:58 +0000

    Mongo lab Included Mongoose

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -14,4 +14,30 @@ router.post('/', async (req, res) => {
     const task = await Task(req.body).save();
     res.status(201).json(task);
 });
+
+// Update Task
+router.put('/:id', async (req, res) => {
+    if (req.body._id) delete req.body._id;
+    const result = await Task.updateOne({
+        _id: req.params.id,
+    }, req.body);
+    if (result.matchedCount) {
+        res.status(200).json({ code:200, msg: 'Task Updated Sucessfully' });
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find Task' });
+    }
+});
+
+// delete Task
+router.delete('/:id', async (req, res) => {
+    if (req.body._id) delete req.body._id;
+    const result = await Task.deleteOne({
+        _id: req.params.id,
+    });
+    if (result.deletedCount) {
+        res.status(204).json();
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find Task' });
+    }
+});
 export default router;
