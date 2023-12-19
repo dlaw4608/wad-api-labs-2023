9b826c3574b349d4d7236fc52e875e657a3285d8
dlaw4608 <dlawton032@gmail.com>
2023-12-19 17:04:46 +0000

    Trending movies from TMDB (Didnt have upcoming from movie-react-labs so used trending instead did all the same steps)

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,9 @@
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
+import {
+    getTrendingMovies
+  } from '../tmdb-api';
 
 const router = express.Router();
 
@@ -18,6 +21,11 @@ router.get('/:id', asyncHandler(async (req, res) => {
     } else {
         res.status(404).json({message: 'The movie you requested could not be found.', status_code: 404});
     }
+}));
+
+router.get('/tmdb/trending', asyncHandler(async (req, res) => {
+    const trendingMovies = await getTrendingMovies();
+    res.status(200).json(trendingMovies);
 }));
 
 export default router;
