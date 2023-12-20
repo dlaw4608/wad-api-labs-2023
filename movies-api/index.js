50c40610faac0e339548643c72662dc2af99ead5
dlaw4608 <dlawton032@gmail.com>
2023-12-20 11:06:52 +0000

    Topic 12 - Get Movies from API

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,24 +1,22 @@
+import authenticate from './authenticate';
+import moviesRouter from './api/movies';   //import movies router
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler'
-import moviesRouter from './api/movies'; 
-import authenticate from './authenticate';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT; 
 
-app.use('/api/movies',authenticate,  moviesRouter);
 app.use(cors());
 app.use(express.json());
+app.use('/api/movies',authenticate,  moviesRouter);
 app.use('/api/users', usersRouter);
 app.use(defaultErrHandler);
-app.use('/api/movies', moviesRouter);
-
 
 
 app.listen(port, () => {
--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,9 +1,13 @@
 export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=6ba077b09d23f842a9241a68f5113d27&language=en-US&include_adult=false&page=1`
-    )
-    return response.json()
-  };
+  const response = await fetch(
+    'http://localhost:8080/api/movies', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  }
+  )
+  return response.json();
+};
 
   export const login = async (username, password) => {
     const response = await fetch('http://localhost:8080/api/users', {
@@ -26,3 +30,4 @@ export const signup = async (username, password) => {
     });
     return response.json();
 };
+
